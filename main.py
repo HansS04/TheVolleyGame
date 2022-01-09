d35c89c25a05c938f917c5d0522422156b005ce9
Han12s <71758705+it1926@users.noreply.github.com>
2022-01-09 15:09:13 +0100

    Main Menu

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-from kivy.config import Config
+from random import random
 
+from kivy.config import Config
+from kivy.properties import NumericProperty, StringProperty
+from kivy.uix.screenmanager import Screen
+
+
+#nastavení do proměních velikost hrací plochy v pixelech
 height = 720
 width = 1280
 
@@ -10,9 +16,23 @@ Config.set('graphics', 'height', height)
 from kivy.app import App
 from kivy.uix.widget import Widget
 
+#třída pro obrazuvku MENU
+class MainWidget(Screen):
+    playerName1 = StringProperty("Player1")
+    playerName2 = StringProperty("Player2")
+    index = 11
+    index2 = 11
+    STATES = ['ARG', 'AUS', 'AUT', 'BEL', 'BLR', 'BRA', 'BUL', 'CAN', 'COL', 'CRO', 'CUB', 'CZE', 'DEN', 'EGY', 'ESP',
+              'EST', 'FIN', 'FRA', 'GBR', 'GER', 'GRE', 'HUN', 'CHN', 'IND', 'IRL', 'IRN', 'ISL', 'ISR', 'ITA', 'JAP',
+              'KOR', 'LAT', 'LIT', 'MAR', 'MEX', 'NED', 'NGR', 'NOR', 'NZL', 'PAK', 'POL', 'POR', 'ROM', 'RUS', 'SLO',
+              'SRB', 'SUI', 'SVK', 'SWE', 'TUN', 'TUR', 'UKR', 'URU', 'USA']
 
-class MainWidget(Widget):
-    pass
+    #definice pro funkci na nastavení jmen jednotlivých hráčů
+    def on_text_validate(self, widget):
+        self.playerName1 = widget.text
+
+    def on_text_validate2(self, widget):
+        self.playerName2 = widget.text
 
 class TheVolleyGameApp(App):
     pass
